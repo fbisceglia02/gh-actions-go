52bc4de0330460a8922a242c0b5621328bf75b0e
Ferruccio Islam Bisceglia <120172848+fbisceglia02@users.noreply.github.com>
2023-09-11 13:55:59 +0000

    chore: disabled person.go, just test artifact

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import "fmt"
-import "public/person.go"
+import "os"
+// import "public/person.go"
 
 // Define a new type named Person
 
@@ -11,9 +12,15 @@ import "public/person.go"
 func main() {
 	fmt.Println("hello from go")
 
-	p := person.Person{name:"Alice",age:20,isAdult:true}
+	// p := person.Person{name:"Alice",age:20,isAdult:true}
 
-	fmt.Println("by ",p.name)
+	// fmt.Println("by ",p.name)
+
+	directory, err := os.Getwd()    //get the current directory using the built-in function
+	if err != nil {
+	   fmt.Println(err) //print the error if obtained
+	}
+	fmt.Println("Current working directory:", directory) //print the required directory	
 }
 
 
